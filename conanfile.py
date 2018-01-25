7265541e7809ffe60839974f7d6a1651656e5ed9
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:53 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostLevel14GroupConan(ConanFile):
     name = "boost_level14group"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_level14group"
-
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = [
         "bimap", "disjoint_sets", "graph", "graph_parallel", "mpi",
         "property_map"]
