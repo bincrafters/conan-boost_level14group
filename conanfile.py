432e773f88d36d8d6fc0a66aadb1940696efdf83
Jerry W <jerrywiltse@gmail.com>
2018-02-04 00:01:21 -0500

    change ver to 1.65.1 after merge

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ from conans import ConanFile, tools
 
 class BoostLevel14GroupConan(ConanFile):
     name = "boost_level14group"
-    version = "1.66.0"
+    version = "1.65.1"
     url = "https://github.com/bincrafters/conan-boost_level14group"
     author = "Bincrafters <bincrafters@gmail.com>"
     exports = ["LICENSE.md"]
@@ -28,55 +28,55 @@ class BoostLevel14GroupConan(ConanFile):
     default_options = "shared=False", "mpicc=default"
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/testing",
-        "boost_algorithm/1.66.0@bincrafters/testing",
-        "boost_any/1.66.0@bincrafters/testing",
-        "boost_array/1.66.0@bincrafters/testing",
-        "boost_assert/1.66.0@bincrafters/testing",
-        "boost_bind/1.66.0@bincrafters/testing",
-        "boost_concept_check/1.66.0@bincrafters/testing",
-        "boost_config/1.66.0@bincrafters/testing",
-        "boost_conversion/1.66.0@bincrafters/testing",
-        "boost_core/1.66.0@bincrafters/testing",
-        "boost_detail/1.66.0@bincrafters/testing",
-        "boost_dynamic_bitset/1.66.0@bincrafters/testing",
-        "boost_exception/1.66.0@bincrafters/testing",
-        "boost_filesystem/1.66.0@bincrafters/testing",
-        "boost_foreach/1.66.0@bincrafters/testing",
-        "boost_function/1.66.0@bincrafters/testing",
-        "boost_functional/1.66.0@bincrafters/testing",
-        "boost_integer/1.66.0@bincrafters/testing",
-        "boost_iterator/1.66.0@bincrafters/testing",
-        "boost_lambda/1.66.0@bincrafters/testing",
-        "boost_lexical_cast/1.66.0@bincrafters/testing",
-        "boost_math/1.66.0@bincrafters/testing",
-        "boost_move/1.66.0@bincrafters/testing",
-        "boost_mpl/1.66.0@bincrafters/testing",
-        "boost_multi_index/1.66.0@bincrafters/testing",
-        "boost_optional/1.66.0@bincrafters/testing",
-        "boost_parameter/1.66.0@bincrafters/testing",
-        "boost_preprocessor/1.66.0@bincrafters/testing",
-        "boost_property_tree/1.66.0@bincrafters/testing",
-        "boost_python/1.66.0@bincrafters/testing",
-        "boost_random/1.66.0@bincrafters/testing",
-        "boost_range/1.66.0@bincrafters/testing",
-        "boost_regex/1.66.0@bincrafters/testing",
-        "boost_serialization/1.66.0@bincrafters/testing",
-        "boost_smart_ptr/1.66.0@bincrafters/testing",
-        "boost_spirit/1.66.0@bincrafters/testing",
-        "boost_static_assert/1.66.0@bincrafters/testing",
-        "boost_system/1.66.0@bincrafters/testing",
-        "boost_test/1.66.0@bincrafters/testing",
-        "boost_throw_exception/1.66.0@bincrafters/testing",
-        "boost_tti/1.66.0@bincrafters/testing",
-        "boost_tuple/1.66.0@bincrafters/testing",
-        "boost_type_traits/1.66.0@bincrafters/testing",
-        "boost_type_traits/1.66.0@bincrafters/testing",
-        "boost_typeof/1.66.0@bincrafters/testing",
-        "boost_unordered/1.66.0@bincrafters/testing",
-        "boost_utility/1.66.0@bincrafters/testing",
-        "boost_variant/1.66.0@bincrafters/testing",
-        "boost_xpressive/1.66.0@bincrafters/testing"
+        "boost_package_tools/1.65.1@bincrafters/testing",
+        "boost_algorithm/1.65.1@bincrafters/testing",
+        "boost_any/1.65.1@bincrafters/testing",
+        "boost_array/1.65.1@bincrafters/testing",
+        "boost_assert/1.65.1@bincrafters/testing",
+        "boost_bind/1.65.1@bincrafters/testing",
+        "boost_concept_check/1.65.1@bincrafters/testing",
+        "boost_config/1.65.1@bincrafters/testing",
+        "boost_conversion/1.65.1@bincrafters/testing",
+        "boost_core/1.65.1@bincrafters/testing",
+        "boost_detail/1.65.1@bincrafters/testing",
+        "boost_dynamic_bitset/1.65.1@bincrafters/testing",
+        "boost_exception/1.65.1@bincrafters/testing",
+        "boost_filesystem/1.65.1@bincrafters/testing",
+        "boost_foreach/1.65.1@bincrafters/testing",
+        "boost_function/1.65.1@bincrafters/testing",
+        "boost_functional/1.65.1@bincrafters/testing",
+        "boost_integer/1.65.1@bincrafters/testing",
+        "boost_iterator/1.65.1@bincrafters/testing",
+        "boost_lambda/1.65.1@bincrafters/testing",
+        "boost_lexical_cast/1.65.1@bincrafters/testing",
+        "boost_math/1.65.1@bincrafters/testing",
+        "boost_move/1.65.1@bincrafters/testing",
+        "boost_mpl/1.65.1@bincrafters/testing",
+        "boost_multi_index/1.65.1@bincrafters/testing",
+        "boost_optional/1.65.1@bincrafters/testing",
+        "boost_parameter/1.65.1@bincrafters/testing",
+        "boost_preprocessor/1.65.1@bincrafters/testing",
+        "boost_property_tree/1.65.1@bincrafters/testing",
+        "boost_python/1.65.1@bincrafters/testing",
+        "boost_random/1.65.1@bincrafters/testing",
+        "boost_range/1.65.1@bincrafters/testing",
+        "boost_regex/1.65.1@bincrafters/testing",
+        "boost_serialization/1.65.1@bincrafters/testing",
+        "boost_smart_ptr/1.65.1@bincrafters/testing",
+        "boost_spirit/1.65.1@bincrafters/testing",
+        "boost_static_assert/1.65.1@bincrafters/testing",
+        "boost_system/1.65.1@bincrafters/testing",
+        "boost_test/1.65.1@bincrafters/testing",
+        "boost_throw_exception/1.65.1@bincrafters/testing",
+        "boost_tti/1.65.1@bincrafters/testing",
+        "boost_tuple/1.65.1@bincrafters/testing",
+        "boost_type_traits/1.65.1@bincrafters/testing",
+        "boost_type_traits/1.65.1@bincrafters/testing",
+        "boost_typeof/1.65.1@bincrafters/testing",
+        "boost_unordered/1.65.1@bincrafters/testing",
+        "boost_utility/1.65.1@bincrafters/testing",
+        "boost_variant/1.65.1@bincrafters/testing",
+        "boost_xpressive/1.65.1@bincrafters/testing"
     )
 
     def package_id_additional(self):
@@ -92,7 +92,7 @@ class BoostLevel14GroupConan(ConanFile):
     short_paths = True
     generators = "boost"
     settings = "os", "arch", "compiler", "build_type"
-    build_requires = "boost_generator/1.66.0@bincrafters/testing"
+    build_requires = "boost_generator/1.65.1@bincrafters/testing"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
