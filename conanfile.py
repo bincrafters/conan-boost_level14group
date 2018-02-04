dbee962a031a6f303e61f3f72fc175339840ad6b
Jerry W <jerrywiltse@gmail.com>
2018-02-04 00:04:42 -0500

    change url to 1_65_1 after merge

--- conanfile.py
+++ conanfile.py
@@ -87,7 +87,7 @@ class BoostLevel14GroupConan(ConanFile):
 
     # BEGIN
 
-    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
     license = "BSL-1.0"
     short_paths = True
     generators = "boost"
